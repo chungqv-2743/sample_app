af3ed98718179acdc9a61ac3539a51e59b3fc0f2
chungqv-2743 <quan.van.chung@sun-asterisk.com>
2021-10-25 06:54:05 +0700

    Chapter 5: Fillter in the layout

--- config/application.rb
+++ config/application.rb
@@ -20,8 +20,8 @@ module SampleApp
     # config.eager_load_paths << Rails.root.join("extras")
     config.middleware.use I18n::JS::Middleware
     config.i18n.load_path += Dir[Rails.root.join("config",
-     "locales", "**","*.{rb,yml}")]
+     "locales", "**", "*.{rb,yml}")]
     config.i18n.available_locales = [:en, :vi]
-    config.i18n.default_locale = :vi
+    config.i18n.default_locale = :en
   end
 end
