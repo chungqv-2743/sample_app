976afaa4fe6a0291d9c1cf37a76c8ddfbb261a87
chungqv-2743 <quan.van.chung@sun-asterisk.com>
2021-10-25 17:58:18 +0700

    Chapter 5: Filling in the layout

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,7 @@ require_relative "boot"
 require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+# you"ve limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module SampleApp
@@ -11,17 +11,14 @@ module SampleApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
     config.middleware.use I18n::JS::Middleware
     config.i18n.load_path += Dir[Rails.root.join("config",
-     "locales", "**","*.{rb,yml}")]
+     "locales", "**", "*.{rb,yml}")]
     config.i18n.available_locales = [:en, :vi]
-    config.i18n.default_locale = :vi
+    config.i18n.default_locale = :en
   end
 end
