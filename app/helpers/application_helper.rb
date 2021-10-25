fa0403e631664fb8f88e15c49e010cef600170a4
chungqv-2743 <quan.van.chung@sun-asterisk.com>
2021-10-25 16:59:08 +0700

    Chapter 4: Rails-flavored  Ruby

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  # Returns the full title
+  def full_title page_title
+    base_title = t("base_title")
+    page_title.blank? ? base_title : [page_title, base_title].join(" | ")
+  end
 end
